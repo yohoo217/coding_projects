8f995a41de5f522d0fc8900a853ce2f0271f779c
yohoo217 <ian.chen@aotter.net>
2023-12-12 13:05:49 +0800

    這個版本是把 room 和 roomrepository 新增回來

---
+++ src/main/kotlin/net/aotter/model/Room.kt
@@ -0,0 +1,9 @@
+package net.aotter.model
+
+data class Room(
+        var id: String?,
+        var title: String?,
+        var mem: String?,
+        var desc: String?,
+        var imgSrc: String?
+)
---
+++ src/main/kotlin/net/aotter/repository/RoomRepository.kt
@@ -0,0 +1,54 @@
+package net.aotter.repository
+
+import com.fasterxml.jackson.databind.MappingIterator
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.dataformat.csv.CsvMapper
+import com.fasterxml.jackson.dataformat.csv.CsvSchema
+import net.aotter.model.Room
+import org.jboss.logging.Logger
+import java.net.URL
+import javax.enterprise.context.ApplicationScoped
+import javax.inject.Inject
+
+@ApplicationScoped
+class RoomRepository {
+
+    @Inject
+    lateinit var logger: Logger
+
+    private val SHEET_URL =
+            "https://docs.google.com/spreadsheets/d/e/2PACX-1vTFSxyakG_CmwMV-mxmVuBft46P6ut897mIvRPn37HPZbunSe9wIg-pcAA5rwIUSuCjqSQoJEle-PL0/pub?output=csv"
+
+    fun getRooms(): List<Room>? = readRemoteCSVMKII(SHEET_URL)
+            ?.map {
+                Room(
+                        it["id"],
+                        it["title"],
+                        it["mem"],
+                        it["desc"],
+                        it["imgSrc"]
+                )
+            }
+
+    fun getRoomTitles(): List<String> {
+        val rooms = getRooms()
+        return rooms?.mapNotNull { it.title } ?: emptyList()
+    }
+
+    fun getRoomPhotos(): MutableList<Map<String, String?>>? = readRemoteCSVMKII(SHEET_URL)
+
+    fun findRoomById(id: String) = getRooms()?.find { it.id == id }
+
+    private fun readRemoteCSVMKII(url: String): MutableList<Map<String, String?>>? {
+        return try {
+            val objectMapper: ObjectMapper = CsvMapper()
+            val schema: CsvSchema = CsvSchema.emptySchema().withHeader()
+            val iterator: MappingIterator<Map<String, String?>> =
+                    objectMapper.readerFor(Map::class.java).with(schema).readValues(URL(url))
+            iterator.readAll()
+        } catch (e: Exception) {
+            logger.warn("readRemoteCSVMKII  failed", e)
+            null
+        }
+    }
+}
